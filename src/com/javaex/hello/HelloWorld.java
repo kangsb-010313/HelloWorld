0540ba1c4927dfb2aabcb66e9698b154733b59e2
kangsb-010313 <kangsb668@naver.com>
2025-04-17 09:49:56 +0900

    1st

---
+++ src/com/javaex/hello/HelloWorld.java
@@ -0,0 +1,18 @@
+/*
+작성자: 강수빈
+작성일: 2025-04-14
+설 명: Hello World 출력하기
+*/
+
+package com.javaex.hello;
+
+public class HelloWorld {
+	
+	public static void main(String[] args){
+		
+        //문자열을 화면에 출력하는 함수
+		System.out.println("Hello World!");
+        
+    }
+	
+}
